d1b5940c9ee5b6e0494e6db737a2a193b465f2de
kirill_barkhatov <k.s.barkhatov@gmail.com>
2024-07-12 01:17:39 +0300

    get_cashback_by_category - готово

---
+++ src/services.py
@@ -0,0 +1,29 @@
+import json
+
+import pandas as pd
+
+from utils import get_transactions_from_xls, date_converter
+
+
+def get_cashback_by_category(data: pd.DataFrame, year: int, month: int) -> str:
+    """Функция позволяет проанализировать, какие категории были наиболее выгодными
+    для выбора в качестве категорий повышенного кешбэка
+    """
+
+    month_data = data.loc[(data["Дата операции"].dt.month == month) & (data["Дата операции"].dt.year == year)]
+    print(month_data)
+    print(month_data.loc[month_data["Кэшбэк"] != 0])
+    cashback_month_data = month_data.loc[month_data["Кэшбэк"] != ""]
+    cashback_grouped = month_data.groupby("Категория")["Кэшбэк"].sum().sort_values(ascending=False).loc[lambda x: x > 0]
+    print()
+    cashback = json.dumps(cashback_grouped.to_dict(),
+        indent=4,
+        ensure_ascii=False
+    )
+    print(cashback)
+
+
+if __name__ == "__main__":
+    df = get_transactions_from_xls()
+    print(df)
+    get_cashback_by_category(df, 2020, 2)
