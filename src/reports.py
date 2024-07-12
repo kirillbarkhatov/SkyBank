391bd69d6cad4d8868b616cc8d73d992c0abea7b
kirill_barkhatov <k.s.barkhatov@gmail.com>
2024-07-12 22:04:07 +0300

    reports - логика написана

--- src/reports.py
+++ src/reports.py
@@ -9,11 +9,18 @@ def spending_by_category(transactions: pd.DataFrame,
                          date: str = None) -> pd.DataFrame:
     """ Функция возвращает траты по заданной категории за последние три месяца (от переданной даты) """
 
-    date64 = date_converter(date)
-    start_date = date64.astype('M8[M]') - np.timedelta64(3, 'M')
+    if date is None:
+        end_date = dt.datetime.now()
+    else:
+        end_date = dt.datetime.strptime(date, "%d.%m.%Y")
+    start_date = end_date.replace(month=((end_date.month + 8) % 12 + 1), hour=0, minute=0, second=0, microsecond=0)
+    print(end_date, start_date)
+    # start_date = date64.astype('M8[M]') - np.timedelta64(3, 'M')
     # start_date = start_date.astype('M8[D]') + np.timedelta64(date64.day(), 'D')
-    print(date64.astype('M8[M]'))
-    df = df.loc[(df['Дата операции'] <= date64) & (df['Дата операции'] >= start_date)]
+    # print(date64.astype('M8[M]'))
+    transactions_by_category = transactions.loc[(transactions['Дата операции'] <= end_date) & (transactions['Дата операции'] >= start_date) & (transactions['Категория'] == category)]
+    print(df)
+
 
 
 
@@ -21,4 +28,4 @@ def spending_by_category(transactions: pd.DataFrame,
 
 if __name__ == "__main__":
     df = get_transactions_from_xls()
-    spending_by_category(df,"Аптеки", "01.05.2020")
+    spending_by_category(df,"Аптеки", "01.04.2020")
